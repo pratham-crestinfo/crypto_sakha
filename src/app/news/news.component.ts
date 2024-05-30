596f34c9744022899c79351771da01f8d9df9433
Pratham Siddhpura <prathamsidd19@gmail.com>
2024-05-30 09:37:05 +0530

    test

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -6,27 +6,33 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [NewscardComponent,CommonModule],
+  imports: [NewscardComponent, CommonModule],
   templateUrl: './news.component.html',
-  styleUrl: './news.component.css'
+  styleUrl: './news.component.css',
 })
 export class NewsComponent {
-constructor(private http:HttpClient){}
-private newsHeaders= new HttpHeaders({
-  'X-RapidAPI-Key': '5273bb30c3msh58e3023355d1531p14ca63jsna27040b80cbe',
-  'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com'
-})
-newsData:any[]=[];
-ngOnInit()
-{
-  this.http.get("https://cryptocurrency-news2.p.rapidapi.com/v1/coindesk",{headers:this.newsHeaders}).subscribe({
-    next:(res:any)=>{
-      console.log(res.data);
-      this.newsData=res.data;
-    },
-    error:(err)=>{
-      console.log(err);
-    },
-  })
-}
+  constructor(private http: HttpClient) {}
+  private newsHeaders = new HttpHeaders({
+    'X-RapidAPI-Key': '5273bb30c3msh58e3023355d1531p14ca63jsna27040b80cbe',
+    'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com',
+  });
+  newsData: any[] = [];
+  ngOnInit() {
+    this.http
+      .get('https://cryptocurrency-news2.p.rapidapi.com/v1/coindesk', {
+        headers: this.newsHeaders,
+      })
+      .subscribe({
+        next: (res: any) => {
+          console.log(res.data);
+          this.newsData = res.data;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+          console.log('completed');
+        },
+      });
+  }
 }
